4ea8f6f8bf03a46cc59e4871a58bf8641ddcb56a
manav0117 <manavthakor0117@gmail.com>
2022-09-19 23:47:56 +0530

    Updates

---
+++ precache-manifest.73141d230e3f0801f270fc4f34122133.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "684153f7466143695d0b6cd9b0373893",
+    "url": "/airbnb-clone/index.html"
+  },
+  {
+    "revision": "5204efd19cd4bb25612c",
+    "url": "/airbnb-clone/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "81a456ef5fd0ebc8cc20",
+    "url": "/airbnb-clone/static/css/main.6777df22.chunk.css"
+  },
+  {
+    "revision": "5204efd19cd4bb25612c",
+    "url": "/airbnb-clone/static/js/2.29c5b276.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/airbnb-clone/static/js/2.29c5b276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81a456ef5fd0ebc8cc20",
+    "url": "/airbnb-clone/static/js/main.584204a3.chunk.js"
+  },
+  {
+    "revision": "f358a9c4d5787a3a04f2",
+    "url": "/airbnb-clone/static/js/runtime-main.fc586be8.js"
+  }
+]);
